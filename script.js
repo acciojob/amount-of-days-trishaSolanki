49e6d7beba16803c515e5bd875709f2e921b3c8f
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-27 15:58:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
 function daysOfAYear(years){
 	function isEapYear(y){
-		return(y % 4 === 0 && y % 4 !== 0) || (y % 400 === 0);
+		return(y % 4 === 0 && y % 100 !== 0) || (y % 400 === 0);
 	}
 	return isLeapYear(years) ? 366 : 365;
+	
 }
